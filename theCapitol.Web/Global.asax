866ccd05dd199031d8eb57135a96faf0ba52d6c9
Derek Duderstadt <derekduderstadt@live.com>
2018-03-13 14:36:26 -0500

    initial commit
    initial commit of base project structure

---
+++ theCapitol.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="theCapitol.Web.MvcApplication" Language="C#" %>
---
+++ theCapitol.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(theCapitol.Web.Startup))]
+namespace theCapitol.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
