6a937417aff9749a730a5019ae007c1d4eea7ecd
Derek Duderstadt <derekduderstadt@live.com>
2018-03-13 19:19:32 -0500

    template conversion
    mid-process template conversion

--- theCapitol.Web/App_Start/BundleConfig.cs
+++ theCapitol.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,22 @@ namespace theCapitol.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
+            //template css (Content/css)
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/font-awesome.min.css",
+                "~/Content/css/magnific-popup.css",
+                "~/Content/css/owl.carousel.css",
+                "~/Content/css/owl.theme.default.css",
+                "~/Content/css/style.css"));
+
+            //template JavaScript (Scripts/js)
+            bundles.Add(new ScriptBundle("~/Scripts/js").Include(
+                "~/Scripts/js/jquery.min.js",
+                "~/Scripts/js/bootstrap.min.js",
+                "~/Scripts/js/jquery.magnific-popup.js",
+                "~/Scripts/js/owl.carousel.min.js",
+                "~/Scripts/js/main.js"));
         }
     }
 }
--- theCapitol.Web/Controllers/HomeController.cs
+++ theCapitol.Web/Controllers/HomeController.cs
@@ -8,7 +8,61 @@ namespace theCapitol.Web.Controllers
 {
     public class HomeController : Controller
     {
+        #region site structure
+        /*
+         * we want a site that is easy to navigate for visitors
+         * unfamiliar to the Capitol. LESS IS MORE
+         * 
+         * Home/Index will contain...
+         * - navigation links (5 total)
+         * - about/welcome
+         * - why choose us (brief overview - what we offer)
+         *  - !IMPORTANT: main service info (Sunday nights @ 6pm)
+         * - testimonials/reviews
+         * - links to recent [blog] articles
+         * ======================================================
+         * NAVIGATION
+         * theCapitol (home, about) || general info
+         * services || service times/ministries (what we offer)
+         * media || pictures & video
+         * team || leadership/who we are
+         * contact || contact form, Google Maps, get involved
+         * blog (*) || articles/resources to read (SEPARATE CONTROLLER)
+         *  - NOTE: this will be linked from media; can update
+         *  main navigation at a later time
+         */
+         //REMOVED ITEMS
+        ////Prices/pricing table - N/A
+        ////Number - N/A
+        #endregion
+
+        //default page (home/about) - theCapitol
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        //services (what we offer)
+        //+ details of small groups/additional ministries
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        //media (pictures, video, podcasts, blog)
+        public ActionResult Media()
+        {
+            return View();
+        }
+
+        //team (who/leadership)
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+        //contact form, Google maps, get involved
+        public ActionResult Contact()
         {
             return View();
         }
